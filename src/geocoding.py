e8697f64b6c81dc23f71fde3eb44fc567d85a20c
VyR-10 <felixandro>
2026-01-08 09:44:07 -0300

    agregada carpeta src

---
+++ src/geocoding.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def georreferenciar(direccion):
+    print("Georreferenciando dirección:", direccion)
+
+    # URL base para la API de geocodificación de Google Maps
+    url = "https://maps.googleapis.com/maps/api/geocode/json"
+    
+    # Parámetros de la solicitud
+    params = {
+        'address': direccion,
+        'key': "AIzaSyAKqyYtw4VH_3LEo0_c1gSv2yfS2PsFxPQ"
+    }
+    
+    # Realizar la solicitud GET
+    respuesta = requests.get(url, params=params)
+
+    
+
+
+    # Verificar si la solicitud fue exitosa
+    if respuesta.status_code == 200:
+        datos = respuesta.json()
+        
+        # Si la respuesta tiene resultados, obtener la latitud y longitud
+        if datos['status'] == 'OK':
+            latitud = datos['results'][0]['geometry']['location']['lat']
+            longitud = datos['results'][0]['geometry']['location']['lng']
+            return latitud, longitud
+        else:
+            return None
+    else:
+        return None
